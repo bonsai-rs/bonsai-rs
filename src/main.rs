ef65eecb5fafdd4c743b955b828ecc69648a9d73
Tom Pekrul <61377248+Disembleergon@users.noreply.github.com>
2025-10-24 23:14:16 +0200

    basic ui (#3)
    * add dependencies
    
    * base ui code
    
    * basic timer ui
    
    * working timer start
    
    * add ability to set the timer
    
    * refactor code and add exit with q

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::{Duration, Instant};
+
+use color_eyre::Result;
+use crossterm::event::{self, KeyCode};
+use ratatui::{
+    DefaultTerminal, Frame,
+    layout::{Constraint, Flex, Layout},
+};
+
+mod widgets;
+use widgets::timer_widget::TimerWidget;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let mut app = App::new();
+    let result = app.run(terminal);
+    ratatui::restore();
+    result
+}
+
+struct App {
+    timer_widget: TimerWidget,
+    quit: bool,
+}
+
+impl App {
+    const fn new() -> Self {
+        App {
+            timer_widget: TimerWidget::new(),
+            quit: false,
+        }
+    }
+
+    pub fn run(&mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        loop {
+            if self.quit {
+                return Result::Ok(());
+            }
+            self.handle_input();
+            self.timer_widget.update();
+            terminal.draw(|frame: &mut Frame| {
+                self.render(frame);
+            })?;
+        }
+    }
+
+    fn handle_input(&mut self) {
+        if event::poll(Duration::from_millis(50)).unwrap() {
+            let e = event::read().unwrap();
+            match e.as_key_event().unwrap().code {
+                KeyCode::Char('q') => self.quit = true,
+                KeyCode::Char(' ') => self.timer_widget.toggle(),
+                KeyCode::Left | KeyCode::Char('h') => {
+                    self.timer_widget.move_selector_left();
+                }
+                KeyCode::Right | KeyCode::Char('l') => {
+                    self.timer_widget.move_selector_right();
+                }
+                KeyCode::Up | KeyCode::Char('j') => {
+                    self.timer_widget.modify_goal_time(1);
+                }
+                KeyCode::Down | KeyCode::Char('k') => self.timer_widget.modify_goal_time(-1),
+                _ => (),
+            };
+        }
+    }
+
+    fn render(&self, frame: &mut Frame) {
+        let horizontal =
+            Layout::horizontal([Constraint::Percentage(50), Constraint::Percentage(50)])
+                .flex(Flex::Center);
+        let [tree_area, timer_area] = horizontal.areas(frame.area());
+        self.timer_widget.render(frame, timer_area);
+    }
 }
---
+++ src/widgets/timer_widget.rs
@@ -0,0 +1,129 @@
+use std::time::{Duration, Instant};
+
+use ratatui::{
+    Frame,
+    layout::{Constraint, Flex, Layout, Rect},
+    widgets::Block,
+};
+use tui_big_text::BigText;
+
+enum TimeSelection {
+    Seconds,
+    Minutes,
+    Hours,
+}
+
+pub struct TimerWidget {
+    timer_startpoint: Option<Instant>,
+    goal_duration: Duration,
+    timer_display: String,
+    time_selector: TimeSelection,
+}
+
+impl TimerWidget {
+    pub const fn new() -> Self {
+        TimerWidget {
+            timer_startpoint: None,
+            goal_duration: Duration::from_secs(45 * 60),
+            timer_display: String::new(),
+            time_selector: TimeSelection::Hours,
+        }
+    }
+
+    pub fn toggle(&mut self) {
+        if self.timer_startpoint.is_some() {
+            self.timer_startpoint = None;
+        } else {
+            self.timer_startpoint = Some(Instant::now());
+        }
+    }
+
+    pub fn modify_goal_time(&mut self, delta: i32) {
+        if self.timer_startpoint.is_some() {
+            return;
+        }
+
+        let new_seconds = match self.time_selector {
+            TimeSelection::Seconds => self
+                .goal_duration
+                .as_secs()
+                .checked_add_signed(delta as i64),
+            TimeSelection::Minutes => self
+                .goal_duration
+                .as_secs()
+                .checked_add_signed(delta as i64 * 60),
+            TimeSelection::Hours => self
+                .goal_duration
+                .as_secs()
+                .checked_add_signed(delta as i64 * 60 * 60),
+        };
+
+        self.goal_duration =
+            Duration::from_secs(new_seconds.unwrap_or(self.goal_duration.as_secs()));
+    }
+
+    pub fn move_selector_left(&mut self) {
+        self.time_selector = match self.time_selector {
+            TimeSelection::Hours => TimeSelection::Seconds,
+            TimeSelection::Minutes => TimeSelection::Hours,
+            TimeSelection::Seconds => TimeSelection::Minutes,
+        }
+    }
+
+    pub fn move_selector_right(&mut self) {
+        self.time_selector = match self.time_selector {
+            TimeSelection::Hours => TimeSelection::Minutes,
+            TimeSelection::Minutes => TimeSelection::Seconds,
+            TimeSelection::Seconds => TimeSelection::Hours,
+        }
+    }
+
+    pub fn update(&mut self) {
+        let elapsed = match self.timer_startpoint {
+            Some(t) => t.elapsed(),
+            None => Duration::from_secs(0),
+        };
+
+        let display_duration = self.goal_duration - elapsed;
+        let seconds = display_duration.as_secs() % 60;
+        let minutes = (display_duration.as_secs() / 60) % 60;
+        let hours = (display_duration.as_secs() / 60) / 60;
+
+        self.timer_display = format!("{:0>2}h {:0>2}min {:0>2}s", hours, minutes, seconds);
+    }
+
+    pub fn render(&self, frame: &mut Frame, area: Rect) {
+        let block = Block::bordered();
+        frame.render_widget(block, area);
+
+        let (upper_selection_row, bottom_selection_row) = if self.timer_startpoint.is_some() {
+            ("", "")
+        } else {
+            match self.time_selector {
+                TimeSelection::Hours => ("^          ", "v          "),
+                TimeSelection::Minutes => ("^", "v"),
+                TimeSelection::Seconds => ("          ^", "          v"),
+            }
+        };
+
+        let timer = BigText::builder()
+            .pixel_size(tui_big_text::PixelSize::Quadrant)
+            .lines(vec![
+                upper_selection_row.into(),
+                self.timer_display.clone().into(),
+                bottom_selection_row.into(),
+            ])
+            .centered()
+            .build();
+
+        let [_, timer_area, _] = Layout::vertical([
+            Constraint::Fill(1),
+            Constraint::Fill(1),
+            Constraint::Fill(1),
+        ])
+        .flex(Flex::Center)
+        .areas(area);
+
+        frame.render_widget(timer, timer_area);
+    }
+}
